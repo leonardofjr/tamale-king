c046ee5fb5c227e11e9c13e1de21002b1e8f7a66
Leo <leonardo.f.jr@gmail.com>
2019-04-23 22:42:30 -0400

    /

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +17,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/app/contact/contact.component.ts
+++ angular/src/app/contact/contact.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Form } from './Form';
+import { MailService } from '../mail.service';
+import { Router } from '@angular/router';
+import { HttpClient} from '@angular/common/http';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+  form: Form;
+  constructor(private http: HttpClient, private mailService: MailService, private location: Location, private router: Router) { }
+  submit: boolean = false;
   ngOnInit() {
+
+  }
+  resolved(captchaResponse: string) {
+    console.log(`Resolved captcha with response ${captchaResponse}:`);
+  }
+
+  disableButton(element) {
+    document.getElementById(element).setAttribute('disabled', '');
+  }
+
+  onSubmit(e, formData) {
+    e.preventDefault();
+    this.mailService.sendMail(formData)
+      .subscribe((data) => {
+        this.router.navigateByUrl('');
+        this.submit = true;
+        this.disableButton('submitBtn')
+      });
   }
 
 }
